fb6178ee2b648e9ff326f548497bc29f62cb542f
Mucahid Yazar <mucahidyazar@gmail.com>
2020-10-31 22:13:14 +0300

    Added vue files

---
+++ tutorials/getting-started-2/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+<div id="counter">Counter: {{ counter }}</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++;
+    }, 1000);
+  },
+};
+</script>
